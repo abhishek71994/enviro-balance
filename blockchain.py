1c774c88180a850c98075770d570f75d1251620e
abhishek71994 <abhishek71994@gmail.com>
2018-03-24 22:26:41 +0800

    node resolving and registration routes

--- blockchain.py
+++ blockchain.py
@@ -146,6 +146,38 @@ def mine():
 	}
 	return jsonify(response), 200
 
+@app.route('/nodes/register',methods=['POST'])
+def register_nodes():
+	value = request.get_json()
+
+	nodes = value.get('nodes')
+	if nodes is None:
+		return "Error: Please supply a valid list of nodes", 400
+
+	for node in nodes:
+		blockchain.register_node(node)
+
+	response = {
+		'message': 'New nodes have been added',
+		'total_nodes': list(blockchain.nodes),
+	}
+	return jsonify(response), 201
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+	replaced = blockchain.resolve_conflicts()
+
+	if replaced:
+		response = {
+ 			'message': 'Our chain was replaced',
+			'new_chain': blockchain.chain
+		}
+	else:
+		response = {
+			'message': 'Our chain is authoritative',
+			'chain': blockchain.chain
+		}
+
+	return jsonify(response), 200
 @app.route('/transactions/new',methods=['POST'])
 def transations_new():
 	values = request.get_json()
